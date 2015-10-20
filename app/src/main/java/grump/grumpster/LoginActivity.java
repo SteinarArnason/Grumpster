8af8450821e637fe9213c8aa673e7e0c806dad2a
kristinn93 <kristinnj13@ru.is>
2015-10-20 19:54:44 +0000

    adding volley for http requests

--- app/src/main/java/grump/grumpster/LoginActivity.java
+++ app/src/main/java/grump/grumpster/LoginActivity.java
@@ -8,6 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends Activity {
     SharedPreferences sp;
 
@@ -33,5 +47,35 @@ public class LoginActivity extends Activity {
         editor.commit();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void loginHttp(View view){
+        System.out.println("heheh http hehehe");
+        String URL = "http://localhost/8000/kristinn";
+        final JSONObject jsonBody;
+        try {
+            jsonBody = new JSONObject("{\"password\":\"api123123\"}");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        HashMap<String, String> params = new HashMap<String, String>();
+
+        JsonObjectRequest req = new JsonObjectRequest(URL, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            VolleyLog.v("Response:%n %s", response.toString(4));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Perror: ", error.getMessage());
+            }
+        });
+        Volley.newRequestQueue(this).add(req);
     }
 }
