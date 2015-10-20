89f2fa662360081154c84f2565eaf96ec5eacb02
kristinn93 <kristinnj13@ru.is>
2015-10-20 21:03:20 +0000

    reverting

--- app/src/main/java/grump/grumpster/MainActivity.java
+++ app/src/main/java/grump/grumpster/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         sp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
     }
+    
 
     @Override
     protected void onResume() {
