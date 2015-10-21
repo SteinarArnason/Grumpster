cf5a742af2d98cc16080f50122ece1714118c025
Steinar Árnason <steinara13@ru.is>
2015-10-21 15:38:30 +0000

    Preference activity now works

--- app/src/main/java/grump/grumpster/MainActivity.java
+++ app/src/main/java/grump/grumpster/MainActivity.java
@@ -1,7 +1,9 @@
 package grump.grumpster;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends LoginActivity {
 
@@ -15,8 +17,11 @@ public class MainActivity extends LoginActivity {
     protected void onResume() {
         super.onResume();
 
-        boolean li = sp.getBoolean("loggedIn", false);
-        if(!li) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("loggedIn", true);
+        editor.commit();
+
+        if(!sp.getBoolean("loggedIn", false)) {
             System.out.println("You are not logged in.");
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
@@ -24,6 +29,11 @@ public class MainActivity extends LoginActivity {
         else {
             System.out.println("You are logged in.");
         }
+    }
+
+    public void settings(View view) {
+        Intent intent = new Intent(this, PreferencesActivity.class);
+        startActivity(intent);
     }
 
 }
