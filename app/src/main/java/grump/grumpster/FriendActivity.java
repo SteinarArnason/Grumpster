31e74f6559b1f6000556d05182ff750920eeab75
kristinn93 <kristinnj13@ru.is>
2015-10-22 22:48:11 +0000

    getting all users works now

--- app/src/main/java/grump/grumpster/FriendActivity.java
+++ app/src/main/java/grump/grumpster/FriendActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -29,25 +31,18 @@ public class FriendActivity extends MainActivity{
         getAllUsers();
     }
 
-    protected void getAllUsers(){
-        JsonObjectRequest req = new JsonObjectRequest
-                (Request.Method.GET, APIprefix+"users", null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        //mTxtDisplay.setText("Response: " + response.toString());
-                        System.out.println("Response : " + response.toString());
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        System.out.println("Error message" + error.getMessage());
-                    }
-                });
-
-        //error.networkResponse.statusCode
-        Volley.newRequestQueue(this).add(req);
+    protected void getAllUsers() {
+        JsonArrayRequest jreq=new JsonArrayRequest(APIprefix + "users", new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                System.out.println("Got response : "  + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error message : " + error.toString());
+            }
+        });
+        Volley.newRequestQueue(this).add(jreq);
     }
 }
