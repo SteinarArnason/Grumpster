c0a3ed7cf3452b0e7f10a4fb4c660e987b1d3dfd
kristinn93 <kristinnj13@ru.is>
2015-10-22 22:33:41 +0000

    getting all friends but has to be array

--- app/src/main/java/grump/grumpster/FriendActivity.java
+++ app/src/main/java/grump/grumpster/FriendActivity.java
@@ -3,13 +3,51 @@ package grump.grumpster;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Gunnar on 21-Oct-15.
  */
-public class FriendActivity extends Activity{
+public class FriendActivity extends MainActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addfriend_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("asdfasdfasdf");
+        getAllUsers();
+    }
+
+    protected void getAllUsers(){
+        JsonObjectRequest req = new JsonObjectRequest
+                (Request.Method.GET, APIprefix+"users", null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //mTxtDisplay.setText("Response: " + response.toString());
+                        System.out.println("Response : " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        System.out.println("Error message" + error.getMessage());
+                    }
+                });
+
+        //error.networkResponse.statusCode
+        Volley.newRequestQueue(this).add(req);
     }
 }
